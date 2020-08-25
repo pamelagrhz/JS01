05e6ec6fc5581c3d33754922f79fc5eb85c9caaf
pamelagrhz <pamelagrhz@gmail.com>
2020-08-24 20:11:34 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require("gulp"),
+    pug = require("gulp-pug"),
+    autoprefixer = require("gulp-autoprefixer"),
+    sass = require("gulp-sass");
+gulp.task("pug", function() {
+    gulp
+        .src("./src/pug/*.pug")
+        .pipe(
+            pug({
+                doctype: "html",
+                pretty: true,
+            })
+        )
+        .pipe(gulp.dest("dist/css"));
+});
+gulp.task("sass", function() {
+    gulp
+        .src("p./src/sass/*.sass")
+        .pipe(
+            sass({
+                outputStyle: "expended",
+                sourceComments: true,
+            })
+        )
+        .pipe(sass())
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 version"],
+            })
+        )
+        .pipe(gulp.dest("./dist/css"));
+});
+gulp.task("watch", function() {
+    gulp.watch("./src/sass/*", ["sass"]);
+    gulp.watch("./src/pug/*.pug", ["pug"]);
+});
+gulp.task("default", ["sass", "pug"]);
